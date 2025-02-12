3559e53967ef2891c326b2564ab0b4651d892a6b
kyrylo <pinchukkg@gmail.com>
2025-02-13 00:59:59 +0200

    added READMEs and module 4 tasks 1-4

--- homework/module_4/task_1/task_1.cpp
+++ homework/module_4/task_1/task_1.cpp
@@ -1,5 +1,28 @@
 #include<iostream>
 
-int main () {
-	std::cout << "test ignore a.out";
+int powsq(int a) {
+	return a*a;
+}
+
+void check_for_combinations(int n) {
+	for (int c = 0; c < n; c++) {
+		for (int a = 0; a < c; a++) {
+			for (int b = 0; b < a; b++) {
+				if (powsq(a) + powsq(b) == powsq(c)) {
+					std::cout << "a: " << a << "; b: " << b << "; c: " << c << '\n';
+				}
+			}
+		}
+	}
+}
+
+
+int main() {
+	while(1) {
+		int n;
+		std::cout << "Enter n: ";
+		std::cin >> n;
+		check_for_combinations(n);
+	}
+	return 0;
 }
