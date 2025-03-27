7e4e4e3bf882ade30c54fa9e43ad7d43f0b6fec0
kyrylo <pinchukkg@gmail.com>
2025-03-27 22:38:25 +0200

    task 6 done

--- homework/module_4/task_6/task_6.cpp
+++ homework/module_4/task_6/task_6.cpp
@@ -1,29 +1,41 @@
 #include <iostream>
 
-const char* first_digit[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-const char* teens_digit[] = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-const char* second_digit[] = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-const char* third_digit[] = {"", "one hundred", "two hundred", "three hundred", "four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred"};
-const char fourth_digit[] = {"one thousand"};
+const char* first_digit[] = {"", "one ", "two ", "three ", "four ", "five ", "six ", "seven ", "eight ", "nine "};
+const char* teens_digit[] = {"ten", "eleven ", "twelve ", "thirteen ", "fourteen ", "fifteen ", "sixteen ", "seventeen ", "eighteen ", "nineteen "};
+const char* second_digit[] = {"", "", "twenty ", "thirty ", "forty ", "fifty ", "sixty ", "seventy ", "eighty ", "ninety "};
+const char* third_digit[] = {"", "one hundred ", "two hundred ", "three hundred ", "four hundred ", "five hundred ", "six hundred ", "seven hundred ", "eight hundred ", "nine hundred "};
+const char fourth_digit[] = {"one thousand "};
 
 
 void num_word(int n) 
 {
+	if (n == 0) 
+	{
+		std::cout << "zero";
+	}
 	if (n == 1000) 
 	{
 		std::cout << fourth_digit;
+		n %= 1000;
 	}
-	else if ((n <= 999 && n >= 100) 
+	if (n <= 999 && n >= 100) 
 	{
-		std::cout << third_digit[n];
+		std::cout << third_digit[n/100];
+		n %= 100;
 	}
-	else if (n <= 99 && n >= 20) 
+	if (n <= 99 && n >= 20) 
 	{
-		std::cout << second_digit[n];
+		std::cout << second_digit[n/10];
+		n %= 10;
 	}
-	else if (n <= 19 && n >= 10)
-			
-	return 0;
+	if (n <= 19 && n >= 10) 
+	{
+		std::cout << teens_digit[n%10];
+	}
+	if (n <= 9 && n >= 1) 
+	{
+		std::cout << first_digit[n];
+	}	
 }
 
 int main() 
@@ -33,7 +45,7 @@ int main()
 		int n;
 		std::cout << "Enter n:";
 		std::cin >> n;
-		const char* fin_word = num_word(n);
-		std::cout << fin_word << '\n';
+		num_word(n);
+		std::cout << '\n';
 	}
 }
